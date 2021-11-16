f02fdb07ce29b5f2eca2d120ba452ee49bd742a1
Firas Saidani <firasaidani@gmail.com>
2021-11-15 21:30:58 -0500

    fin

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
+"""main.py"""
 import squadro
 import api
-
-argument  = squadro.traiter_la_ligne_de_commande()
-
+argument = squadro.traiter_la_ligne_de_commande()
 partie_jeu = api.créer_une_partie(argument.IDUL)
 partie_en_cours = api.récupérer_une_partie(partie_jeu[0])
 while True:
-   
     print(squadro.afficher_le_plateau_de_jeu(partie_en_cours[2]))
-    print('donner le numéro du pion ',partie_en_cours[1] )
+    print('donner le numéro du pion ',partie_en_cours[1])
     pion = input()
     partie_en_cours = api.jouer_un_coup(partie_en_cours[0], partie_en_cours[1], pion)
     partie_en_cours = api.récupérer_une_partie(partie_en_cours[0])
-
-
-
-
-
-
