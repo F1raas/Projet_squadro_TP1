329a936dba4b09f48e5f6c26d33077e323b4be8b
Firas Saidani <firasaidani@gmail.com>
2021-11-15 22:32:57 -0500

    tt

--- squadro.py
+++ squadro.py
@@ -1,10 +1,8 @@
 import argparse
 
 def traiter_la_ligne_de_commande():
-   parser = argparse.ArgumentParser(add_help=True, 
-   description='Squadro')
-   parser.add_argument('IDUL',nargs ='+', type = str,
-   metavar='IDUL',help = 'IDUL du ou des joueur(s)')
+   parser = argparse.ArgumentParser(add_help=True, description='Squadro')
+   parser.add_argument('IDUL',nargs ='+', type = str, metavar='IDUL',help = 'IDUL du ou des joueur(s)')
    parser.add_argument('-p','--parties',metavar='',type = list,help = 'Lister les 20 dernières parties')
    return parser.parse_args()
 
